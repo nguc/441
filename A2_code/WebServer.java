d2a8ee02f828bca7137e948d98d9d5869b144483
nguc <c.ngu.91@gmail.com>
2017-10-18 16:24:08 -0600

    Worker thread class added

--- A2_code/WebServer.java
+++ A2_code/WebServer.java
@@ -21,15 +21,15 @@ import java.text.SimpleDateFormat;
 	
 
 public class WebServer extends Thread {
-	String METHOD = "GET"; 
-	String PROTOCOL = "HTTP/1.1";
-	String CONNECTION_CLOSED = "Connection: close\r\n";
+	
+	
+	public int PORT_NUM;
 	
 	private volatile boolean shutdown = false;
 	private ServerSocket serverSocket;
 	
-	PrintWriter out;
-	BufferedReader in;
+	
+	
 	
 	//int POOL_SIZE = 8;
 	//ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE);
@@ -42,17 +42,16 @@ public class WebServer extends Thread {
      */
 	public WebServer(int port) {
 		// initialization
-		if (port > 1024 && port < 65536 ) 
+		try 
 		{
-			try 
+			if (port > 1024 && port < 65536 ) 
 			{
 				// Initialize and open a server socket that listens to port number given
-				this.serverSocket = new ServerSocket(port);
+				PORT_NUM = port;
 				System.out.println("Started server on port " + port);
 			} 
-			catch (IOException e) { System.out.println("Error opening socket: " + e);}
-				
-		}	
+		}
+		catch (Exception e) { System.out.println("Error opening socket: " + e);}	
 	}
 
 	
@@ -63,72 +62,38 @@ public class WebServer extends Thread {
 	 * 
      */
 	public void run() {
-		Socket clientSocket = null;
-		try 
-		{
-			clientSocket = serverSocket.accept();
-			serverSocket.setSoTimeout(1000);
-			
-			while (!shutdown) 
-			{
-				// accept new connection
-				clientSocket = serverSocket.accept();
-				// create worker thread to handle new connection
-				//Thread thread = new WebServer(clientSocket.getPort());
-				//thread.start();
-			}
-			
-			out = new PrintWriter (clientSocket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			String inputLine, outputLine = "Hello";
-			String [] wholeRequest, reqLine;
-			out.println(outputLine);
-				
-			while ((inputLine = in.readLine()) != null)
-			{
-				wholeRequest = inputLine.split("\r\n");
-				reqLine = wholeRequest[0].split(" ");
-				// get a status code
-				String code = isValidRequest(reqLine);
-				if (code.equals("200 OK"))
-				{
-					// check if have the file
-					File file = new File(reqLine[1]);
-					if(file.isDirectory()) 
-					{
-						// send server response + "Connection:close" at end
-						// tranfer file over connection
-						// close connection
-						System.out.println(errorResponse(code));
-					}
-					else 
-					{
-						// return error
-						code = "404 Not Found";
-					}
-				}
-				
-				
-				// send error for bad request (400) or not found (404)
-				// Date\n Server\n connection:close
-				System.out.println(errorResponse(code));
-				
-				// clean up (close sockets, threads, etc.)
-				in.close();
-				out.close();
-				serverSocket.close();
-				
-			}
-		}
-		catch (IOException e) 
-		{ 
-			// do nothing, allows process to check the shutdown flag	
-		}	
-		
 
+		try
+		{
+			this.serverSocket = new ServerSocket(PORT_NUM);
+			serverSocket.setSoTimeout(1000);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error opening server socket");
+		}		
 		
-		
+		while (!shutdown) 
+		{
+			Socket clientSocket = new Socket();
+			try 
+			{
+			// accept new connection
+				try { clientSocket = serverSocket.accept();}
+				catch (SocketTimeoutException e){} 
+			// create worker thread to handle new connection
+			Thread thread = new Thread((new Worker(clientSocket)));
+			thread.start();
+			}
+			catch (IOException e) 
+			{ 
+				// do nothing, allows process to check the shutdown flag	
+			}		
+		}
+			
+			
 	}
+		
 	
 	
 	
@@ -138,80 +103,17 @@ public class WebServer extends Thread {
      */
 	public void shutdown() {
 		shutdown = true;
-	}
-
-	/**
-	 *  Checks if request was sent properly
-	 */
-	public String isValidRequest(String [] reqLine) {
-		String code = "200 OK";
-		
-		if (reqLine.length != 3)
-			code = "400 Bad Request";
-		else if (!reqLine[0].equals(METHOD))
-			code = "400 Bad Request";
-		else if (!reqLine[2].equals(PROTOCOL))
-			code = "400 Bad Request";
-		
-		return code;
-	}
-	
-	public String goodResponse(String code) {
-		String response = 
-				METHOD + " " + code + "\r\n" + 
-				getDate() +
-				getServer() + 
-				// last-mod
-				// content-length
-				// content-type
-				CONNECTION_CLOSED;
-		return response;
-	}
-	
-	/**
-	 * 
-	 * @param code
-	 * @return
-	 */
-	public String errorResponse(String code) {
-		String response = METHOD + " " + code + "\r\n" + 
-						  getDate() + 
-						  getServer() + 
-						  CONNECTION_CLOSED;
-		return response;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public String getDate () {
-		String current = "";
-		DateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz");
-		Date date = new Date();
-		current =  dateFormat.format(date);
-		return current + "\r\n";
-	}
-	
-	/**
-	 * Code taken from stack overflow: 
-	 * https://stackoverflow.com/questions/7883542/getting-the-computer-name-in-java
-	 * @return
-	 */
-	public String getServer() {
-		String hostname = "";
 		try
 		{
-		    InetAddress addr;
-		    addr = InetAddress.getLocalHost();
-		    hostname = addr.getHostName();
+			this.serverSocket.close();
 		}
-		catch (UnknownHostException ex)
-		{
-		    System.out.println("Hostname can not be resolved");
+		catch (Exception e){
+			
 		}
-		return hostname + "\r\n";
+		
 	}
+
+	
 	
 	
 	/**
--- A2_code/Worker.java
+++ A2_code/Worker.java
@@ -1,10 +1,151 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Worker implements Runnable{
-
+	public final String METHOD = "GET"; 
+	public final String PROTOCOL = "HTTP/1.1";
+	public final String CONNECTION_CLOSED = "Connection: close\r\n";
+	public Socket socket;
+	
+	
+	public Worker(Socket socket) {
+		this.socket = socket;
+	}
+	
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		try
+		{
+			PrintWriter out = new PrintWriter (socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String inputLine, outputLine = "Hello";
+			String [] wholeRequest, reqLine;
+			out.println(outputLine);
+				
+			while ((inputLine = in.readLine()) != null)
+			{
+				System.out.println(inputLine);
+				wholeRequest = inputLine.split("\r\n");
+				reqLine = wholeRequest[0].split(" ");
+				// get a status code
+				String code = isValidRequest(reqLine);
+				if (code.equals("200 OK"))
+				{
+					// check if have the file
+					System.out.println("file: " + reqLine[1].substring(1));
+					File file = new File(//current directory + reqLine[1]);
+					if(file.exists()) 
+					{
+						// send server response + "Connection:close" at end
+						// tranfer file over connection
+						// close connection
+						System.out.println(errorResponse(code));
+					}
+					else 
+					{
+						// return error
+						code = "404 Not Found";
+					}
+				}
+				
+				
+				// send error for bad request (400) or not found (404)
+				// Date\n Server\n connection:close
+				System.out.println(errorResponse(code));
+				
+				// clean up (close sockets, threads, etc.)
+				in.close();
+				out.close();
+				
+				
+			}
+		}
+		catch (Exception e){
+			
+		}
 		
+	}
+	
+	
+	/**
+	 *  Checks if request was sent properly
+	 */
+	public String isValidRequest(String [] reqLine) {
+		String code = "200 OK";
+		
+		if (reqLine.length != 3)
+			code = "400 Bad Request";
+		else if (!reqLine[0].equals(METHOD) || !reqLine[2].equals(PROTOCOL))
+			code = "400 Bad Request";
+		
+		return code;
+	}
+	
+	public String goodResponse(String code) {
+		String response = 
+				PROTOCOL + " " + code + "\r\n" + 
+				getDate() +
+				getServer() + 
+				// last-mod
+				// content-length
+				// content-type
+				CONNECTION_CLOSED;
+		return response;
+	}
+	
+	/**
+	 * 
+	 * @param code
+	 * @return
+	 */
+	public String errorResponse(String code) {
+		String response = PROTOCOL + " " + code + "\r\n" + 
+						  getDate() + 
+						  getServer() + 
+						  CONNECTION_CLOSED;
+		return response;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getDate () {
+		String current = "";
+		DateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz");
+		Date date = new Date();
+		current =  dateFormat.format(date);
+		return current + "\r\n";
+	}
+	
+	/**
+	 * Code taken from stack overflow: 
+	 * https://stackoverflow.com/questions/7883542/getting-the-computer-name-in-java
+	 * @return
+	 */
+	public String getServer() {
+		String hostname = "";
+		try
+		{
+		    InetAddress addr;
+		    addr = InetAddress.getLocalHost();
+		    hostname = addr.getHostName();
+		}
+		catch (UnknownHostException ex)
+		{
+		    System.out.println("Hostname can not be resolved");
+		}
+		return hostname + "\r\n";
 	}
 
 }
